d9599186e57869b828a5e293c7c49006e7852262
diya-thabet <mohamaddhia@gmail.com>
2025-07-22 21:30:51 +0100

    ui

--- app/src/main/java/com/dhia/unitconverter/MainActivity.kt
+++ app/src/main/java/com/dhia/unitconverter/MainActivity.kt
@@ -9,11 +9,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -23,6 +28,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dhia.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,10 +59,41 @@ fun UnitConverter(modifier: Modifier = Modifier) {
             text = "Unit Converter",
 
         )
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField("", {})
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
 
-            Button(onClick = { }) {
+
+            Box{
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(
+                        Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down"
+                    )
+                }
+                // drop down menu
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text ={ Text(text = "Centimeters") },
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text ={ Text(text = "Meters") },
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text ={ Text(text = "Feet") },
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text ={ Text(text = "Milimeters") },
+                        onClick = {}
+                    )
+                }
+            }
+                Spacer(modifier = Modifier.width(27.dp))
                 Box{
                     Button(onClick = {}) {
                         Text("Select")
@@ -65,22 +102,34 @@ fun UnitConverter(modifier: Modifier = Modifier) {
                             contentDescription = "Arrow Down"
                         )
                     }
-                }
-                Box{
-                    Button(onClick = {}) {
-                        Text("Select")
-                        Icon(
-                            Icons.Default.ArrowDropDown,
-                            contentDescription = "Arrow Down"
+                    // drop down menu
+                    DropdownMenu(expanded = false, onDismissRequest = {}) {
+                        DropdownMenuItem(
+                            text ={ Text(text = "Centimeters") },
+                            onClick = {}
+                        )
+                        DropdownMenuItem(
+                            text ={ Text(text = "Meters") },
+                            onClick = {}
+                        )
+                        DropdownMenuItem(
+                            text ={ Text(text = "Feet") },
+                            onClick = {}
+                        )
+                        DropdownMenuItem(
+                            text ={ Text(text = "Milimeters") },
+                            onClick = {}
                         )
                     }
                 }
             }
-        }
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Result: ")
+        }
+
     }
 
-}
+
 
 @Preview(showBackground = true)
 @Composable
